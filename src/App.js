712f0dba2992126c6a737522e9d1e55d0f19ab02
POloznikovKonstantin <Kost.Poloznikov.ya@gmail.com>
2023-08-09 21:28:30 +0300

    Create card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useLocalStorage} from './hooks';
 import { Header } from './Components/Header/Header';
 import { useTranslation } from 'react-i18next';
+import { Card } from './Components/UI/Card/Card.jsx';
 
 export const App = () => {
   const {i18n} = useTranslation();
@@ -16,6 +17,7 @@ export const App = () => {
   return (
     <main className='container'>
         <Header lng={language} changeLng={changeLanguage}/>
+        <Card></Card>
     </main>
   )
 }
--- src/Components/UI/Card/Card.jsx
+++ src/Components/UI/Card/Card.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import './Card.css';
 
-export const Card = (nameHeader, description, placeHolder, imgLink, link, stack) => {
+export const Card = ({nameHeader, description, placeHolder, imgLink, link, stack}) => {
     return(
-        <div className="Background-Card">
-            <img src={imgLink} alt=""></img>
-            <div className="Border-Stack">
+        <div className='background-card'>
+            <img src={imgLink} alt=''/>
+            <div className='border-stack'>
                 <p>{stack}</p>
             </div>
             <h4>{nameHeader}</h4>
             <p>{description}</p>
-            <a href={link}>{placeHolder}</a>
+            <a className='button-link' href={link}>Open to GitHUB</a>
         </div>
     )
 }
